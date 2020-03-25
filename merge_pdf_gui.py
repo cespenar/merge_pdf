db1d0636d6cb13c0de1e8259b54f69e946eb828d
cespenar <cespenar1@gmail.com>
2020-03-25 13:58:05 +0100

    small changes

--- merge_pdf_gui.py
+++ merge_pdf_gui.py
@@ -1,31 +1,15 @@
 #!/usr/bin/env python3
 
-import argparse
 import glob
 import os
-import sys
 import PySimpleGUI as sg
 from PyPDF2 import PdfFileMerger, PdfFileReader
 
 
-def get_arguments(options=sys.argv[1:]):
-
-    parser = argparse.ArgumentParser(conflict_handler="resolve", \
-        description="Merge pdf files in a directory.")
-    parser.add_argument("-i", "--input_dir", type=str, required=True, \
-        help="The directory containing the files.")
-    parser.add_argument("-o", "--output", type=str, required=True, \
-        help="Output file.")
-    parser.add_argument("-p", "--pattern", type=str, default="*.pdf", \
-        help="The pattern for glob to select the files Default: \".pdf\".")
-    args = parser.parse_args(options)
-    return args
-
-
-def merge_files(input_dir, pattern):
+def merge_files(input_dir):
 
     merged_pdf = PdfFileMerger()
-    for f_name in glob.glob(os.path.join(input_dir, pattern)):
+    for f_name in glob.glob(os.path.join(input_dir, "*.pdf")):
         merged_pdf.append(PdfFileReader(f_name, "rb"))
     
     return merged_pdf
@@ -33,30 +17,31 @@ def merge_files(input_dir, pattern):
 
 if __name__ == "__main__":
 
-    # args = get_arguments()
-
-    sg.theme("DarkAmber")	# Add a touch of color
-    # All the stuff inside your window.
+ 
+    sg.theme("System Default 1")
+    
     layout = [ 
-            # [sg.Text("Folder with PDF files to merge")],
-            # [sg.FolderBrowse()],
-            # [sg.Text("Output file")],
-            # [sg.popup_get_file("Choose the output file", default_extension="pdf", save_as=True)],
-            [sg.Text("Input folder", size=(12, 1)), sg.Input(), sg.FolderBrowse()],
-            [sg.Text("Output file", size=(12, 1)), sg.Input(), sg.FileBrowse()],
-            [sg.Button("Merge files"), sg.Button("Cancel")],
+            [sg.Text("Input folder", size=(12, 1)), sg.Input(),
+                sg.FolderBrowse(initial_folder=os.getcwd(), key="input", tooltip="Select folder containing pdf files for merging.")],
+            [sg.Text("Output file", size=(12, 1)), sg.Input(),
+                sg.FileSaveAs(file_types=(("PDF", "*.pdf"),), initial_folder=os.getcwd(), key="output", tooltip="Select the output file.")],
+            [sg.Button("Merge files", key="button_merge", tooltip="Merge selected files"), sg.Cancel(tooltip="Cancel")],
             ]
 
-    # Create the Window
     window = sg.Window("Merge PDFs", layout)
-    # Event Loop to process "events" and get the "values" of the inputs
+    
     while True:
         event, values = window.read()
-        if event in (None, "Cancel"):	# if user closes window or clicks cancel
+        print(values)
+        if event == "button_merge":
+            if not values["input"] or not values["output"]:
+                sg.popup("You need to choose both input folder and output file!", title="Warning!")
+            else:
+                merged_pdf = merge_files(values["input"])
+                merged_pdf.write(values["output"])
+                sg.popup("Files have been merged!", title="Success!")
+                break
+        if event in (None, "Cancel"):
             break
-        print('Values ', values[0], values[1])
 
     window.close()
-
-    merged_pdf = merge_files(args.input_dir, args.pattern)
-    # merged_pdf.write(args.output)
