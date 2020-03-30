6b44e3dbc490065d819bef6a8e57a44bf41d69be
cespenar <cespenar1@gmail.com>
2020-03-30 11:58:27 +0200

    Reformated the code

--- merge_pdf_gui.py
+++ merge_pdf_gui.py
@@ -9,30 +9,32 @@ def merge_files(input_dir):
     merged_pdf = PdfFileMerger()
     for f_name in glob.glob(os.path.join(input_dir, "*.pdf")):
         merged_pdf.append(PdfFileReader(f_name, "rb"))
-    
+
     return merged_pdf
-    
+
 
 if __name__ == "__main__":
 
     sg.theme("System Default 1")
-    
-    layout = [ 
-            [sg.Text("Input folder", size=(12, 1)), sg.Input(),
-                sg.FolderBrowse(initial_folder=os.getcwd(), key="input", tooltip="Select folder containing pdf files for merging.")],
-            [sg.Text("Output file", size=(12, 1)), sg.Input(),
-                sg.FileSaveAs(file_types=(("PDF", "*.pdf"),), initial_folder=os.getcwd(), key="output", tooltip="Select the output file.")],
-            [sg.Button("Merge files", key="button_merge", tooltip="Merge selected files"), sg.Cancel(tooltip="Cancel")],
-            ]
+
+    layout = [
+        [sg.Text("Input folder", size=(12, 1)), sg.Input(),
+         sg.FolderBrowse(initial_folder=os.getcwd(), key="input", tooltip="Select folder containing pdf files for merging.")],
+        [sg.Text("Output file", size=(12, 1)), sg.Input(),
+         sg.FileSaveAs(file_types=(("PDF", "*.pdf"),), initial_folder=os.getcwd(), key="output", tooltip="Select the output file.")],
+        [sg.Button("Merge files", key="button_merge",
+                   tooltip="Merge selected files"), sg.Cancel(tooltip="Cancel")],
+    ]
 
     window = sg.Window("Merge PDFs", layout)
-    
+
     while True:
         event, values = window.read()
         print(values)
         if event == "button_merge":
             if not values["input"] or not values["output"]:
-                sg.popup("You need to choose both input folder and output file!", title="Warning!")
+                sg.popup(
+                    "You need to choose both input folder and output file!", title="Warning!")
             else:
                 merged_pdf = merge_files(values["input"])
                 if not values["output"].lower().endswith((".pdf")):
